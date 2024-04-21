ef1c79d97e80697a2a03e64f386676aa6877636f
Rafael Souza <rpsouza.dev@gmail.com>
2024-04-21 08:15:05 -0300

    Add first commit

---
+++ gestao_vagas/src/main/kotlin/com/rpsouza/GestaoVagasApplication.kt
@@ -0,0 +1,11 @@
+package com.rpsouza
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestaoVagasApplication
+
+fun main(args: Array<String>) {
+	runApplication<GestaoVagasApplication>(*args)
+}
---
+++ gestao_vagas/src/main/kotlin/com/rpsouza/modules/candidate/controllers/CandidateController.kt
@@ -0,0 +1,4 @@
+package com.rpsouza.modules.candidate.controllers
+
+class CandidateController {
+}
