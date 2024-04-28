34edfd75459faeb7b53dbbd8de72bbb9f16f799b
Rafael Souza <rpsouza.dev@gmail.com>
2024-04-28 15:38:54 -0300

    Add swagger

--- gestao_vagas/src/main/kotlin/com/rpsouza/GestaoVagasApplication.kt
+++ gestao_vagas/src/main/kotlin/com/rpsouza/GestaoVagasApplication.kt
@@ -1,11 +1,20 @@
 package com.rpsouza
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+  info = Info(
+    title = "Gestão de vagas",
+    description = "API responsável pela gestão de vagas",
+    version = "1"
+  )
+)
 @SpringBootApplication
 class GestaoVagasApplication
 
 fun main(args: Array<String>) {
-	runApplication<GestaoVagasApplication>(*args)
+  runApplication<GestaoVagasApplication>(*args)
 }
