cd94d85318dcbed66df420381adc9fc29666ff02
Rafael Souza <rpsouza.dev@gmail.com>
2024-04-29 21:45:47 -0300

    Add examples in requests

--- gestao_vagas/src/main/kotlin/com/rpsouza/modules/company/controllers/JobController.kt
+++ gestao_vagas/src/main/kotlin/com/rpsouza/modules/company/controllers/JobController.kt
@@ -3,6 +3,14 @@ package com.rpsouza.modules.company.controllers
 import com.rpsouza.modules.company.dto.CreateJobDTO
 import com.rpsouza.modules.company.model.JobEntity
 import com.rpsouza.modules.company.useCases.CreateJobUseCase
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,6 +32,18 @@ class JobController {
 
   @PostMapping("/")
   @PreAuthorize("hasRole('COMPANY')")
+  @Tag(name = "Vagas", description = "Informações das vagas")
+  @Operation(
+    summary = "Cadastro de vagas",
+    description = "Essa rota é responsavel por cadastrar as vagas dentro da empresa"
+  )
+  @ApiResponses(
+    ApiResponse(
+      responseCode = "200",
+      content = [Content(schema = Schema(implementation = JobEntity::class))]
+    )
+  )
+  @SecurityRequirement(name = "jwt_auth")
   fun create(
     @Valid @RequestBody createJobDTO: CreateJobDTO,
     request: HttpServletRequest
--- gestao_vagas/src/main/kotlin/com/rpsouza/modules/company/dto/CreateJobDTO.kt
+++ gestao_vagas/src/main/kotlin/com/rpsouza/modules/company/dto/CreateJobDTO.kt
@@ -1,7 +1,24 @@
 package com.rpsouza.modules.company.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class CreateJobDTO(
+
+  @Schema(
+    example = "Junior",
+    requiredMode = Schema.RequiredMode.REQUIRED
+  )
   val level: String,
+
+  @Schema(
+    example = "Gympass, Plano de saúde",
+    requiredMode = Schema.RequiredMode.REQUIRED
+  )
   val benefits: String,
+
+  @Schema(
+    example = "Vaga para pessoa desenvolvedora Kotlin junior",
+    requiredMode = Schema.RequiredMode.REQUIRED
+  )
   val description: String,
 )
