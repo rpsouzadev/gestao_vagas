efcd34c3f45c5386b11549ee7c64641d0b31685c
Rafael Souza <rpsouza.dev@gmail.com>
2024-05-02 20:13:14 -0300

    Add apply job success test

--- gestao_vagas/src/main/kotlin/com/rpsouza/modules/candidate/useCases/ApplyJobCandidateUseCase.kt
+++ gestao_vagas/src/main/kotlin/com/rpsouza/modules/candidate/useCases/ApplyJobCandidateUseCase.kt
@@ -2,8 +2,10 @@ package com.rpsouza.modules.candidate.useCases
 
 import com.rpsouza.exceptions.JobNotFoundException
 import com.rpsouza.exceptions.UserNotFoundException
+import com.rpsouza.modules.candidate.model.ApplyJobEntity
 import com.rpsouza.modules.candidate.repository.ApplyJobRepository
 import com.rpsouza.modules.candidate.repository.CandidateRepository
+import com.rpsouza.modules.company.repositories.JobRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.UUID
@@ -15,13 +17,13 @@ class ApplyJobCandidateUseCase {
   private lateinit var candidateRepository: CandidateRepository
 
   @Autowired
-  private lateinit var jobRepository: CandidateRepository
+  private lateinit var jobRepository: JobRepository
 
   @Autowired
   private lateinit var applyJobRepository: ApplyJobRepository
 
 
-  fun invoke(idCandidate: UUID, idJob: UUID) {
+  fun invoke(idCandidate: UUID, idJob: UUID): ApplyJobEntity {
     candidateRepository.findById(idCandidate).orElseThrow {
       throw UserNotFoundException()
     }
@@ -29,5 +31,9 @@ class ApplyJobCandidateUseCase {
     jobRepository.findById(idJob).orElseThrow {
       throw JobNotFoundException()
     }
+
+    val applyJobEntity = ApplyJobEntity(candidateId = idCandidate, jobId = idJob)
+
+    return applyJobRepository.save(applyJobEntity)
   }
 }
